b0561caf9d4c8b2c2b96b5433ce0871b0073dbd0
ordo001 <113034616+ordo001@users.noreply.github.com>
2025-05-07 22:51:36 +0300

    init

---
+++ ClanControlPanel.Api/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using ClanControlPanel.Core.DTO;
+using ClanControlPanel.Core.Interfaces.Services;
+using ClanControlPanel.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClanControlPanel.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IUserServices userServices) : ControllerBase
+    {
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] AuthRequest authRequest)
+        {
+            var token = await userServices.Login(authRequest.Login, authRequest.Password);
+            if (token is null)
+            {
+                return NotFound();
+            }
+
+            HttpContext.Response.Cookies.Append("JwtMonster",token);
+
+            return Ok(token);
+        }
+
+        [HttpGet("validateToke")]
+        [Authorize]
+        public async Task<IActionResult> ValidateToken()
+        {
+            return Ok(new {isValid = true});
+        }
+
+        [HttpPost("logout")]
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            Response.Cookies.Delete("JwtMonster");
+            // Реализовать сервис по добавлению токена в черный список, для избежания повторного его использования
+            return Ok();
+        }
+
+
+       
+    }
+}
---
+++ ClanControlPanel.Api/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using ClanControlPanel.Application.Servises;
+using ClanControlPanel.Core.DTO;
+using ClanControlPanel.Core.Interfaces.Services;
+using ClanControlPanel.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiСontrolPanel.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController(IUserServices userServise, IConfiguration config) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllUser()
+        {
+
+            var userList = await userServise.GetAllUser();
+
+            return Ok(userList);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveUser(int id)
+        {
+            await userServise.RemoveUserById(id);
+            return Ok();
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserRequest registerUserRequest)
+        {
+            var validationResult = registerUserRequest.Validation(registerUserRequest);
+            if (validationResult.Any())
+            {
+                return BadRequest(validationResult);
+            }
+
+            var result = await userServise.Register(
+                registerUserRequest.Login,
+                registerUserRequest.Password,
+                registerUserRequest.Name);
+
+            if (result is null)
+            {
+                return BadRequest("Логин занят");
+            }
+            return Ok(result);
+        }
+
+        [HttpPatch("update")]
+        public async Task<IActionResult> Update([FromBody] UpdateUserRequest updateUserRequest)
+        {
+            var task = userServise.UpdateUser(updateUserRequest.Id, updateUserRequest.Name, updateUserRequest.Login, updateUserRequest.Password);
+            try
+            {
+                await task;
+                //var result = Task.WhenAny(task);
+                //Task.WhenAny(task);
+                //if(result.Exception is not null)
+                    //throw result.Exception.InnerException;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest(task.Exception?.InnerException?.Message);
+            }
+            // TODO: Сделать обработку исключений, но до этого создать кастомные
+            //return Ok();
+        }
+    }
+}
